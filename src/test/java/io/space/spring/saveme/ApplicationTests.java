0bc83942f9644b161d34c85248ef7ab589e9f063
Jurica Migač <jurica1.migac@gmail.com>
2021-09-07 17:42:18 +0200

    #11efmhk : Initial Spring project

---
+++ src/test/java/io/space/spring/saveme/ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.space.spring.saveme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
